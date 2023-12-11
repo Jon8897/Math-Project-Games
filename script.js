788a585cf26da4c87930ba1100c5d1b204004b40
jon8897 <jontykeefe8@gmail.com>
2023-12-11 20:07:16 +0000

    V0.0.1.1 - creating version numbers for introduction of new changes in development

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// V0.0.1.1
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function () {
